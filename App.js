5ccb140f567a805d835c78cdd0da6cdad8d77c76
Rohan <rohanrajput16.phy@gmail.com>
2020-11-07 15:44:54 +0530

    stack-navigation Done

--- App.js
+++ App.js
@@ -1,15 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button} from 'react-native';
+import {createAppContainer, createStackNavigator} from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class HomeScreen extends React.Component {
+  render(){
+    return(
+
+      <View style={styles.container}>
+    <Text>React Home Screen !</Text>
+    <Button
+      title="Go to About page"
+      onPress={()=>this.props.navigation.navigate('About')}
+    />
+    <StatusBar style="auto" />
+  </View>
+      )
+  }
 }
+
+class AboutScreen extends React.Component{
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text>About Screen Here We go !!!</Text>
+        <StatusBar style="auto"/>
+      </View>
+    )
+  }
+}
+const AppNavigator = createStackNavigator({
+  Home:{
+    screen:HomeScreen
+  },
+  About:{
+    screen:AboutScreen
+  }
+})
+export default createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
   container: {
